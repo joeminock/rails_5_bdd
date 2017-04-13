966c8c7913ab37f0e3bcd036e501231c4eb835df
Joe Minock <joe.minock@gmail.com>
2017-04-13 10:53:30 -0600

    Update articles controller, fix bugs in article new view, add notices to template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,20 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = article.new
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash[:success] = "Article has been created"
+    redirect_to articles_path
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+  
 end
