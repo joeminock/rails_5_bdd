2d0b820496404f48edbeeb8699f7e293c5bbb98a
Joe Minock <joe.minock@gmail.com>
2017-04-14 09:15:21 -0600

    Update Articles, new view to include errors

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -18,4 +18,21 @@ RSpec.feature "Creating Articles" do
     #Articles Path
     expect(page.current_path).to eq(articles_path)
   end
+
+  scenario "A user fails to create a new article" do
+    #Visit Root page
+    visit "/"
+    #Click on New Articles
+    click_link "New Article"
+    #Fill in the Title
+    fill_in "Title", with: ""
+    #Fill in the body
+    fill_in "Body", with: ""
+    #Create Article
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
 end
